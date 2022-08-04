dc0e3d317980c2b7056e70ca26049c6dac56fb39
pss-coder <pawandeepse@me.com>
2022-08-04 11:13:36 +0200

    Initial Commit

---
+++ Minatic/MinaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinaticApp.swift
+//  Minatic
+//
+//  Created by Pawandeep Singh on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MinaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
